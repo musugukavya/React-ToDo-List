12f3024cf63f14175827492f5de1f8116a1d6f91
musugukavya <2reachkavyamusugu@gmail.com>
2023-06-06 12:23:56 +0530

    React ToDo list

---
+++ todo/src/NewTodoForm.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+export function NewTodoForm({ onSubmit }) {
+  const [newItem, setNewItem] = useState("")
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (newItem === "") return
+
+    onSubmit(newItem)
+
+    setNewItem("")
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="new-item-form">
+      <div className="form-row">
+        <h1>To-Do List</h1>
+        <h2><label htmlFor="item">Add New Item</label></h2>
+        <input
+          value={newItem}
+          placeholder="Add new item by typing here"
+          onChange={e => setNewItem(e.target.value)}
+          type="text"
+          id="item"
+        />
+      </div>
+      <button className="btn">Add</button>
+    </form>
+  )
+}
